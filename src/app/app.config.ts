77d06a95bd21e8d73fe26bb57ceb43e48c6e5141
Abhishek Singh <prince9singh@gmail.com>
2020-03-14 15:39:51 +0530

    init fastcon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 export const App: any = {
-  store: "Hbuys", // change this with your app name
-  url: "https://hbuys.net/", // change this with your URL (please use https, recommended)
-  consumerKey: "ck_656e4187a5dbcb654eaac107db172194313cfd61", // change this with your Consumer Key from WooCommerce
-  consumerSecret: "cs_114b0b7310ce0adce541035a84290e7105c6fe90", // change this with your Consumer Secret from WooCommerce
+  store: "Fastcon Shopping", // change this with your app name
+  url: "https://fastconshopping.com/", // change this with your URL (please use https, recommended)
+  consumerKey: "ck_74914f9720a4ca7e21184ed9985e561fe9a15633", // change this with your Consumer Key from WooCommerce
+  consumerSecret: "cs_01292748594c0b97a31bab9582bded0309e82e53", // change this with your Consumer Secret from WooCommerce
   IosAppId: "", // FOR IOS RATING USE APPLE ID from appstoreconnect
 
   languages: [
