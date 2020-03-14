cff9e5a2624c96462b1df42842c495b18995d7fd
Abhishek Singh <prince9singh@gmail.com>
2020-03-14 14:21:58 +0530

    Some fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 export const App: any = {
-  store: "WooApp", // change this with your app name
-  url: "https://app.democontentphoeniixx.com/", // change this with your URL (please use https, recommended)
-  consumerKey: "ck_63eb7c5f8b7d4f8de8c0741255a2880066171a3b", // change this with your Consumer Key from WooCommerce
-  consumerSecret: "cs_94e849d1184e8a964bdfec7bdc3c09d7c131024e", // change this with your Consumer Secret from WooCommerce
+  store: "Hbuys", // change this with your app name
+  url: "https://hbuys.net/", // change this with your URL (please use https, recommended)
+  consumerKey: "ck_656e4187a5dbcb654eaac107db172194313cfd61", // change this with your Consumer Key from WooCommerce
+  consumerSecret: "cs_114b0b7310ce0adce541035a84290e7105c6fe90", // change this with your Consumer Secret from WooCommerce
   IosAppId: "", // FOR IOS RATING USE APPLE ID from appstoreconnect
 
   languages: [
--- src/providers/woocommerce/woocommerce.ts
+++ src/providers/woocommerce/woocommerce.ts
@@ -1,29 +1,34 @@
-import { SettingsProvider } from './../settings/settings';
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import * as WC from 'woocommerce-api';
-import { App } from '../../app/app.config';
-import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import { SettingsProvider } from "./../settings/settings";
+import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import * as WC from "woocommerce-api";
+import { App } from "../../app/app.config";
+import { HttpClient } from "@angular/common/http";
+import "rxjs/add/operator/map";
 
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 @Injectable()
 export class WooCommerceProvider {
   WooCommerce: any;
   woo: any = {};
   RestURL: string;
-  constructor(public storage: Storage, public http: HttpClient, public temp: SettingsProvider) {
+  constructor(
+    public storage: Storage,
+    public http: HttpClient,
+    public temp: SettingsProvider
+  ) {
     this.RestURL = App.url + "/wp-json/wc/v2/";
     this.init();
   }
 
   init() {
-    this.WooCommerce = WC({
+    this.WooCommerce = new WooCommerceRestApi({
       url: App.url,
       consumerKey: App.consumerKey,
       consumerSecret: App.consumerSecret,
       wpAPI: true, //or false if you want to use the legacy API v3
-      version: 'wc/v2', //or wc/v1
+      version: "wc/v2", //or wc/v1
       //verifySsl: true,
       queryStringAuth: true
     });
@@ -35,31 +40,36 @@ export class WooCommerceProvider {
   //   let pp = "?per_page=" + per_page;
   //   let o = order ? "&order=" + order : '';
   //   let ob = orderby ? "&orderby=" + orderby : '';
-  //   return this.WooCommerce.getAsync("products/categories" + pp + pt + o + ob).then((x) => {
-  //     console.log(JSON.parse(x.body));
-  //     return JSON.parse(x.body);
+  //   return this.WooCommerce.get("products/categories" + pp + pt + o + ob).then((x) => {
+  //     console.log(x.data);
+  //     return x.data;
   //   });
   // }
-  getAllCategories(parent?) {//: Promise<any> {
-    let pt = parent ? "?parent=" + parent : '';
+  getAllCategories(parent?) {
+    //: Promise<any> {
+    let pt = parent ? "?parent=" + parent : "";
     return this.http.get(this.RestURL + "products/all-categories" + pt);
   }
-  getSubCategories(c: any) {//: Promise<any> {
-    return this.WooCommerce.getAsync("products/categories?parent=" + c).then((x) => {
-      console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+  getSubCategories(c: any) {
+    //: Promise<any> {
+    return this.WooCommerce.get("products/categories?parent=" + c).then(x => {
+      //console.log(x.data);
+      return x.data;
     });
   }
 
   loadSetting() {
-    return this.WooCommerce.getAsync('system_status').then((x) => {
-      this.temp.setSettings(JSON.parse(x.body).settings, 'settings');
-      return JSON.parse(x.body).settings;
-    }, err => {
-      console.log(err);
-    });
+    return this.WooCommerce.get("system_status").then(
+      x => {
+        console.log(x);
+        this.temp.setSettings(x.data.settings, "settings");
+        return x.data.settings;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-
 
   // getAllProducts(page: number, category: number, search: string, min_price: number, max_price: number, per_page: number, status: string, order: string, attribute: string = '', attribute_term: string = '', include: string, orderby: string, featured: boolean, on_sale: boolean, visible: boolean) {//: Promise<any> {
   //   let p = page ? '&page=' + page : '';
@@ -77,109 +87,138 @@ export class WooCommerceProvider {
   //   let f = featured ? '&featured=' + featured : '';
   //   let os = on_sale ? '&on_sale=' + on_sale : '';
   //   let v = visible ? '&visible=' + visible : '';
-  //   return this.WooCommerce.getAsync("products" + s + p + pp + c + q + min + max + a + t + o + i + ob + f + os + v).then((x) => {
-  //     console.log(JSON.parse(x.body));
-  //     return JSON.parse(x.body);
+  //   return this.WooCommerce.get("products" + s + p + pp + c + q + min + max + a + t + o + i + ob + f + os + v).then((x) => {
+  //     console.log(x.data);
+  //     return x.data;
   //   });
   // }
-  getProductById(include?, id?) {//Multiple ids suppoerted(include=1,2,3,4,5) or single id supported(id=1)
+  getProductById(include?, id?) {
+    //Multiple ids suppoerted(include=1,2,3,4,5) or single id supported(id=1)
     let incl = include ? "?include=" + include : "";
     let i = id ? "?id=" + id : "";
     return this.http.get(this.RestURL + "get-products-by-id" + incl + i);
   }
   search(search, per_page?) {
     let s = search ? "?search=" + search : "?search=''";
-    let pp = per_page ? "&per_page=" + per_page : '';
+    let pp = per_page ? "&per_page=" + per_page : "";
     return this.http.get(this.RestURL + "custom-search" + s + pp);
   }
-  getCustomProducts(page, per_page, sort, category, search, min_price?, max_price?, on_sale?, featured?, filterData?) {
-    let p = page ? '?page=' + page : '?page=1';
-    let pp = per_page ? '&per_page=' + per_page : '&per_page=10';
-    let s = sort ? '&sort=' + sort : '&sort=default';
-    let c = category ? '&category=' + category : '';
-    let sh = search ? '&search=' + search : '';
-    let min_p = min_price ? '&min_price=' + min_price : '';
-    let max_p = max_price ? '&max_price=' + max_price : '';
-    let os = on_sale ? '&on_sale=' + on_sale : '';
-    let f = featured ? '&featured=' + featured : '';
+  getCustomProducts(
+    page,
+    per_page,
+    sort,
+    category,
+    search,
+    min_price?,
+    max_price?,
+    on_sale?,
+    featured?,
+    filterData?
+  ) {
+    let p = page ? "?page=" + page : "?page=1";
+    let pp = per_page ? "&per_page=" + per_page : "&per_page=10";
+    let s = sort ? "&sort=" + sort : "&sort=default";
+    let c = category ? "&category=" + category : "";
+    let sh = search ? "&search=" + search : "";
+    let min_p = min_price ? "&min_price=" + min_price : "";
+    let max_p = max_price ? "&max_price=" + max_price : "";
+    let os = on_sale ? "&on_sale=" + on_sale : "";
+    let f = featured ? "&featured=" + featured : "";
     console.log("custom-products" + p + pp + s + c + sh + min_p + max_p);
-    return this.http.post(this.RestURL + "custom-products" + p + pp + s + c + sh + min_p + max_p + os + f, filterData);
+    return this.http.post(
+      this.RestURL +
+        "custom-products" +
+        p +
+        pp +
+        s +
+        c +
+        sh +
+        min_p +
+        max_p +
+        os +
+        f,
+      filterData
+    );
   }
   getProductThumb(id) {
     return this.http.get(this.RestURL + "product/thumbnail?id=" + id);
   }
   getAttributeTerms(id: number) {
-    return this.WooCommerce.getAsync("products/attributes/" + id + "/terms?hide_empty=true").then((x) => {
-      // console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+    return this.WooCommerce.get(
+      "products/attributes/" + id + "/terms?hide_empty=true"
+    ).then(x => {
+      // console.log(x.data);
+      return x.data;
     });
   }
 
+  getAttributes() {
+    //: Promise<any> {
 
-  getAttributes() {//: Promise<any> {
-
-    return this.WooCommerce.getAsync("products/attributes").then((x) => {
-      // console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+    return this.WooCommerce.get("products/attributes").then(x => {
+      // console.log(x.data);
+      return x.data;
     });
   }
   getProductAttributes(id?: number) {
     let i = id ? "?id=" + id : "";
-    return this.WooCommerce.getAsync("products/product-attributes/" + i).then((x) => {
-      // console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+    return this.WooCommerce.get("products/product-attributes/" + i).then(x => {
+      // console.log(x.data);
+      return x.data;
     });
   }
   getCustomAttributes(product?) {
     let p = product ? "&product=" + product : "";
-    return this.http.get(this.RestURL + "products/custom-attributes?hide_empty=true" + p);
+    return this.http.get(
+      this.RestURL + "products/custom-attributes?hide_empty=true" + p
+    );
   }
 
-
-  getProductVariation(data) {  //Custom api made by phoeniixx
+  getProductVariation(data) {
+    //Custom api made by phoeniixx
     return this.http.post(this.RestURL + "products/get-variation", data);
   }
   getOrders(id: number, page: number, per_page: number) {
-    let p = page ? '&page=' + page : '';
-    let pp = per_page ? '&per_page=' + per_page : '&per_page=10';
-    return this.WooCommerce.getAsync('orders?customer=' + id + p + pp).then((data) => {
-      return JSON.parse(data.body);
+    let p = page ? "&page=" + page : "";
+    let pp = per_page ? "&per_page=" + per_page : "&per_page=10";
+    return this.WooCommerce.get("orders?customer=" + id + p + pp).then(data => {
+      return data.data;
     });
   }
   updateOrder(id, status, transaction_id?) {
     var data = {
       status: status,
-      transaction_id: transaction_id || ''
+      transaction_id: transaction_id || ""
     };
-    return this.WooCommerce.putAsync('orders/' + id, data).then((data) => {
-      return JSON.parse(data.body);
+    return this.WooCommerce.put("orders/" + id, data).then(data => {
+      return data.data;
     });
   }
   getOrder(order_id: number) {
-    return this.WooCommerce.getAsync('orders/' + order_id).then((data) => {
-      return JSON.parse(data.body);
+    return this.WooCommerce.get("orders/" + order_id).then(data => {
+      return data.data;
     });
   }
   getDownloads(id: number) {
-    return this.WooCommerce.getAsync("customers/" + id + "/downloads").then((x) => {
-      // console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+    return this.WooCommerce.get("customers/" + id + "/downloads").then(x => {
+      // console.log(x.data);
+      return x.data;
     });
   }
   getUserInfo(id: number) {
-    return this.WooCommerce.getAsync("customers/" + id).then((x) => {
-      // console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
+    return this.WooCommerce.get("customers/" + id).then(x => {
+      // console.log(x.data);
+      return x.data;
     });
   }
   updateUserInfo(id: number, data) {
-    return this.WooCommerce.postAsync("customers/" + id, data).then((x) => {
-      return JSON.parse(x.body);
+    return this.WooCommerce.post("customers/" + id, data).then(x => {
+      return x.data;
     });
   }
   createOrder(data) {
-    return this.WooCommerce.postAsync("orders", data).then((x) => {
-      return JSON.parse(x.body);
+    return this.WooCommerce.post("orders", data).then(x => {
+      return x.data;
     });
   }
   saveAppSettings() {
@@ -188,7 +227,9 @@ export class WooCommerceProvider {
   checkPincode(pincode, product_id?) {
     let p: any = {};
     p.pincode = pincode;
-    if (product_id) { p.product_id = product_id }
+    if (product_id) {
+      p.product_id = product_id;
+    }
     return this.http.post(this.RestURL + "checkpincode", p);
   }
 
@@ -197,50 +238,52 @@ export class WooCommerceProvider {
   }
 
   getCoupons() {
-    return this.WooCommerce.getAsync("coupons").then((x) => {
-      return JSON.parse(x.body);
-    })
+    return this.WooCommerce.get("coupons").then(x => {
+      return x.data;
+    });
   }
   getReviewSettings(product_id, user_id) {
     let p_id = product_id ? "?product_id=" + product_id : "?";
     let u_id = user_id ? "&user_id=" + user_id : "";
 
-    return this.http.get(this.RestURL + "product/review-settings" + p_id + u_id).subscribe((x) => {
-      this.temp.setSettings(x, 'reviewSettings');
-      return x;
-    })
+    return this.http
+      .get(this.RestURL + "product/review-settings" + p_id + u_id)
+      .subscribe(x => {
+        this.temp.setSettings(x, "reviewSettings");
+        return x;
+      });
   }
   getProductReviews(id: number, status = "approved") {
-    let WCv3 = WC({
+    let WCv3 = new WooCommerceRestApi({
       url: App.url,
       consumerKey: App.consumerKey,
       consumerSecret: App.consumerSecret,
       wpAPI: true, //or false if you want to use the legacy API v3
-      version: 'wc/v3', //or wc/v1
+      version: "wc/v3", //or wc/v1
       //verifySsl: true,
       queryStringAuth: true
     });
-    let p = id ? "?product=" + id : '';
+    let p = id ? "?product=" + id : "";
     let s = "&status=" + status;
-    return WCv3.getAsync("products/reviews" + p + s).then((x) => {
-      console.log(JSON.parse(x.body));
-      return JSON.parse(x.body);
-    })
+    return WCv3.get("products/reviews" + p + s).then(x => {
+      console.log(x.data);
+      return x.data;
+    });
   }
   postProductReviews(data) {
-    let WCv3 = WC({
+    let WCv3 = new WooCommerceRestApi({
       url: App.url,
       consumerKey: App.consumerKey,
       consumerSecret: App.consumerSecret,
       wpAPI: true, //or false if you want to use the legacy API v3
-      version: 'wc/v3', //or wc/v1
-      //verifySsl: true, 
+      version: "wc/v3", //or wc/v1
+      //verifySsl: true,
       queryStringAuth: true
     });
 
-    return WCv3.postAsync("products/reviews", data).then((x) => {
-      return JSON.parse(x.body);
-    })
+    return WCv3.post("products/reviews", data).then(x => {
+      return x.data;
+    });
   }
   getTermConditon() {
     return this.http.get(this.RestURL + "terms");
