e74ec599dde05786fee170e1633c097ebf3f9d95
Abhishek Singh <abhishek@webiixx.com>
2020-06-10 10:26:52 +0530

    Fastcon update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 export const App: any = {
   store: "Fastcon Shopping", // change this with your app name
   url: "https://fastconshopping.com/", // change this with your URL (please use https, recommended)
-  consumerKey: "ck_74914f9720a4ca7e21184ed9985e561fe9a15633", // change this with your Consumer Key from WooCommerce
-  consumerSecret: "cs_01292748594c0b97a31bab9582bded0309e82e53", // change this with your Consumer Secret from WooCommerce
+  consumerKey: "ck_701fbf326967bc32034f0021becd110fd50dc96c", // change this with your Consumer Key from WooCommerce
+  consumerSecret: "cs_4151a64ccc00b7a6752816ee25f8f94713e23873", // change this with your Consumer Secret from WooCommerce
   IosAppId: "", // FOR IOS RATING USE APPLE ID from appstoreconnect
 
   languages: [
     //Don't Change multilanguage is in beta
     { id: "en", name: "English" },
     { id: "hi", name: "हिन्दी" },
-    { id: "ar", name: "عربى" }
+    { id: "ar", name: "عربى" },
   ],
-  defaultLang: "en" //Don't Change multilanguage is in beta
+  defaultLang: "en", //Don't Change multilanguage is in beta
 };
