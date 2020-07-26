6eb5161e4e71996e33972d7f10bdb2998a43e081
Abhishek Singh <abhishek@webiixx.com>
2020-07-26 10:13:34 +0530

    push

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 export const App: any = {
   store: "Fastcon Shopping", // change this with your app name
   url: "https://fastconshopping.com", // change this with your URL (please use https, recommended)
-  consumerKey: "ck_701fbf326967bc32034f0021becd110fd50dc96c", // change this with your Consumer Key from WooCommerce
-  consumerSecret: "cs_4151a64ccc00b7a6752816ee25f8f94713e23873", // change this with your Consumer Secret from WooCommerce
+  consumerKey: "ck_6b7a5e6186d705a2b57edc3d1a7c5a35cbfb80e3", // change this with your Consumer Key from WooCommerce
+  consumerSecret: "cs_2ac1d942826cd61ac2e50cedf0eac8f275aeeb3c", // change this with your Consumer Secret from WooCommerce
   IosAppId: "", // FOR IOS RATING USE APPLE ID from appstoreconnect
 
   languages: [
