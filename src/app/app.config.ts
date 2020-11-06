917d9f646f24146f468dfaefa60b726ce748a230
Kamlesh Gangwar <kamlesh@webiixx.com>
2020-11-06 21:14:50 +0530

    Fix nothing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 export const App: any = {
   store: "Fastcon Shopping", // change this with your app name
   url: "https://fastconshopping.com", // change this with your URL (please use https, recommended)
-  consumerKey: "ck_2b84819b7b5a54159b3b2a894225083e5b114bd7", // change this with your Consumer Key from WooCommerce
-  consumerSecret: "cs_b42f0a811fa2229b52365ff7f0bfb526a6b96592", // change this with your Consumer Secret from WooCommerce
+  consumerKey: "ck_6f8d732eb1d0c4221bd8568fc3f77ece1497dbab", // change this with your Consumer Key from WooCommerce
+  consumerSecret: "cs_c8a0a0e6b60e8047716772e2f35888aca5b7b4fb", // change this with your Consumer Secret from WooCommerce
   IosAppId: "", // FOR IOS RATING USE APPLE ID from appstoreconnect
 
   languages: [
